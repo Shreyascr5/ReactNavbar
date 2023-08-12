c02acd06e205b4ca05c9395005fc0eef6af8053a
Shreyascr5 <shreyas050801@gmail.com>
2023-08-12 20:39:16 +0530

    imported links from data file

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,38 +15,25 @@ const Navbar = () => {
         </div>
         <div className="links-container show-container">
           <ul className="links">
-            <li>
-              <a href="#">
-                home
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                about
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                contact 
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                products
-              </a>
-            </li>
+           {links.map((link)=>{
+            const {id,url,text}=link;
+            return(
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            )
+           })}
           </ul>
         </div>
         <ul className="social-icons">
-          <li><a href="https://www.twitter.com">
-            <FaTwitter/>
-          </a></li>
-          <li><a href="https://www.instagram.com">
-            <FaInstagram/>
-          </a></li>
-          <li><a href="https://www.facebook.com">
-            <FaFacebook/>
-          </a></li>
+          {social.map((socials)=>{
+            const{id,url,icon}=socials;
+            return(
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })}
         </ul>
       </div>
     </nav>
