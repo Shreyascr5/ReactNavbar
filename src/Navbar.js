8d0ab3272d609ce1bf86e20f80cc8b1ea681e15f
Shreyascr5 <shreyas050801@gmail.com>
2023-08-12 22:56:30 +0530

    added the auto height for the margin

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,17 +4,33 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
+  const[showLink,setShowLink]=useState(false);
+  const linksContainerRef=useRef(null);
+  const linksRef=useRef(null);
+  useEffect(()=>{
+    const navHeight=linksRef.current.getBoundingClientRect().height;
+    console.log(navHeight);
+    if(showLink){
+      linksContainerRef.current.style.height=`${navHeight}px`;
+    }else{
+      linksContainerRef.current.style.height='0p';
+    }
+    
+  },[showLink])
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={()=>{
+            setShowLink(!showLink);
+          }}>
             <FaBars/>
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
+        
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
            {links.map((link)=>{
             const {id,url,text}=link;
             return(
